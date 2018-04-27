93d25392cccb8cf6314bad30a3f808d7b0c19f39
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-04-27 19:09:14 -0400

    Added project

--- ARSpaceDraw/ViewController.swift
+++ ARSpaceDraw/ViewController.swift
@@ -14,8 +14,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var gestureNode:SCNNode!
+    
+    var lastPoint = CGPoint.zeroPoint
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -24,10 +29,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
+        //let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         // Set the scene to the view
         sceneView.scene = scene
+        
+        addTapGestureToSceneView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,7 +42,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = [.horizontal, .vertical]
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -51,6 +59,97 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
+    
+    
+    func addTapGestureToSceneView() {
+//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addShipToSceneView(withGestureRecognizer:)))
+//        sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.swipeHandler(_:)))
+        sceneView.addGestureRecognizer(swipeGesture)
+    }
+    
+    @objc func addShipToSceneView(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        guard let hitTestResult = hitTestResults.first else { return }
+        let transform = hitTestResult.worldTransform
+        let position = SCNVector3.init(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+        
+            //SCNVector3.positionFrom(matrix: hitTestResult.worldTransform)
+        
+        guard let shipScene = SCNScene(named: "art.scnassets/ship.scn"),
+            let shipNode = shipScene.rootNode.childNode(withName: "ship", recursively: false)
+            else {
+                print("Couldn't find node")
+                return
+        }
+        shipNode.position = position
+        sceneView.scene.rootNode.addChildNode(shipNode)
+        print("Added ship node")
+    }
+
+    @objc func swipeHandler(_ gestureRecognizer : UISwipeGestureRecognizer) {
+        print("Swipe handler")
+        let tapLocation = gestureRecognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        guard let hitTestResult = hitTestResults.first else {
+            print("No hit test results")
+            return
+        }
+        let transform = hitTestResult.worldTransform
+        let position = SCNVector3.init(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+        
+        print("Gesture recongizer state: \(gestureRecognizer.state.rawValue)")
+//        if gestureRecognizer.state == .recognized{
+//            print("recognized gesture state")
+//        }
+//        else
+        if gestureRecognizer.state == .began {
+            let node = createSphere()
+            node.position = position
+            sceneView.scene.rootNode.addChildNode(node)
+        }
+        else if gestureRecognizer.state == .ended {
+            print("Gesture recognizer ended")
+            // Perform action.
+            let node = createSphere()
+            node.position = position
+            sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    // MARK: touches
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        // Keep track of where the user started (aka when finger hits the "canvas")
+        let lastPoint = touch.location(in: self.view)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else{
+            return
+            
+        }
+        let currentPoint = touch.location(in: view)
+        //drawLineFrom(lastPoint, toPoint: currentPoint)
+        
+        lastPoint = currentPoint
+        
+    }
+    
+    
+    func createSphere()->SCNNode{
+        let sphere = SCNSphere(radius: 0.05)
+        //sphere.firstMaterial?.fillMode = .lines
+        let node = SCNNode(geometry: sphere)
+        return node
+    }
+
 
     // MARK: - ARSCNViewDelegate
     
@@ -62,6 +161,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // 1
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+        
+        // 3
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
